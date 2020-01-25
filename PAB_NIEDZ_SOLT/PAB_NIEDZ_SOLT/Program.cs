3e227c32ae115966916f695b97886f9fbdbd6a9e
Wojtek <nwojtek@gmail.com>
2020-01-25 09:08:23 +0100

    Drugie podejscie

---
+++ PAB_NIEDZ_SOLT/PAB_NIEDZ_SOLT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PAB_NIEDZ_SOLT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
